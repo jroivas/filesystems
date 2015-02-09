d52362c03c468320e0a2959e2166797eb2cc4566
Jouni Roivas <jroivas@iki.fi>
2015-02-09 23:34:55 +0200

    Cleanup

--- clothesfs.hh
+++ clothesfs.hh
@@ -22,66 +22,6 @@ public:
     virtual uint32_t sectorSize() const = 0;
 };
 
-#if 0
-class ClothesListing
-{
-public:
-    ClothesListing(uint8_t *data, uint32_t size)
-        : m_data(data),
-        m_size(size),
-        m_items(NULL),
-        m_continues(false)
-    {
-        parse();
-    }
-    ~ClothesListing();
-
-    uint32_t entries() const;
-
-    uint8_t type(uint32_t index) const;
-    const char *filename(uint32_t index) const;
-    const char *contents(uint32_t index) const;
-    uint32_t size(uint32_t index) const;
-
-    bool write(uint8_t *data, uint32_t size);
-    bool continues() const
-    {
-        return m_continues;
-    }
-
-protected:
-    bool parse();
-    bool detect();
-    uint32_t parseFile(uint32_t pos, uint8_t type);
-    uint32_t parseItem(uint8_t type, uint32_t pos);
-
-    class Item {
-    public:
-        Item()
-            :
-            type(0),
-            name(NULL),
-            inode(0),
-            next(NULL)
-        {}
-
-        uint8_t type;
-        char *name;
-        uint32_t inode;
-        //const char *contents;
-        //uint32_t size;
-
-        Item *next;
-    };
-    uint8_t *m_data;
-    uint32_t m_size;
-    Item *m_items;
-    bool m_continues;
-
-    void append(Item *item);
-};
-#endif
-
 class ClothesFS
 {
 public:
